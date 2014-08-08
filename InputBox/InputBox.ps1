04168e1a9174805a89e2c34030cafa720eaedb13
lrector <lrector@gmail.com>
2014-08-08 15:54:15 -0500

    Remove some result scripts

--- InputBox/InputBox.ps1
+++ InputBox/InputBox.ps1
@@ -1,4 +1,23 @@
-[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
-$computer = [Microsoft.VisualBasic.Interaction]::InputBox("Enter a computer name", "Computer", "$env:computername")
+# Show an Open Folder Dialog and return the directory selected by the user.
+function Read-FolderBrowserDialog([string]$Message, [string]$InitialDirectory, [switch]$NoNewFolderButton)
+{
+    $browseForFolderOptions = 0
+    if ($NoNewFolderButton) { $browseForFolderOptions += 512 }
+ 
+    $app = New-Object -ComObject Shell.Application
+    $folder = $app.BrowseForFolder(0, $Message, $browseForFolderOptions, $InitialDirectory)
+    if ($folder) { $selectedDirectory = $folder.Self.Path } else { $selectedDirectory = '' }
+    [System.Runtime.Interopservices.Marshal]::ReleaseComObject($app) > $null
+    return $selectedDirectory
+}
 
-[Microsoft.VisualBasic.Interaction]::MsgBox("$Computer", 0,"This is a message box")
+#[System.Reflection.Assembly]::LoadWithPartialName('Microsoft.VisualBasic') | Out-Null
+#$computer = [Microsoft.VisualBasic.Interaction]::InputBox("Enter a computer name", "Computer", "$env:computername")
+
+#[Microsoft.VisualBasic.Interaction]::MsgBox("$Computer", 0,"Verify")
+
+$directoryPath = Read-FolderBrowserDialog -Message "Please select a directory" -InitialDirectory 'C:\' -NoNewFolderButton
+if (![string]::IsNullOrEmpty($directoryPath)) { Write-Host "You selected the directory: $directoryPath" }
+else { "You did not select a directory." }
+
+[Microsoft.VisualBasic.Interaction]::MsgBox("$directoryPath",0,"Path Selected")
